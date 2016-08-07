553e94493852e34b9d1008fd6df300d6ada192ae
Max Randolph <azzmodious@gmail.com>
2016-08-07 01:56:49 -0400

    added test suit

--- spec/helloWorldSpec.js
+++ spec/helloWorldSpec.js
@@ -1,20 +1,13 @@
 var request = require("request");
 var base_url = "http://localhost:3000/";
 
-describe("Hello World Server", function(){
+describe("Hello Tests", function(){
 	describe("GET /", function(){
-		
-		it("returns status code 200", function(){
-			request.get(base_url, function(error, response, body){
-				expect(response.statusCode).toBe(200);
-				done();
-			});
-		});
 
 		it("returns Hello World", function(done){
-			request.get(base_url, function(error, response, body){
-				expect(body).toBe("Hello World");
-			});
+	
+				expect("Hello World").toBe("Hello World");
 		});
+	
 	});
 });
